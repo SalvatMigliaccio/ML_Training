62c25d7fbe583254f2d3b18c242d0496bdec6fcb
Salvatore Migliaccio <s.migliaccio@namirial.com>
2025-08-01 12:35:53 +0200

    Enhance data processing script: add feature scaling, train-test split, and print statements for debugging

--- Part_1_Data_PreProcessing/Data_processing.py
+++ Part_1_Data_PreProcessing/Data_processing.py
@@ -16,8 +16,8 @@ y = dataset.iloc[:, -1].values  # Target variable
 # Handling missing data
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 x[:, 1:3] = imputer.fit_transform(x[:, 1:3])
-print("Features after handling missing data:")
-print(x)
+# print("Features after handling missing data:")
+# print(x)
 
 # Encoding categorical data
 #encoding the Independent Variable
@@ -25,5 +25,37 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])] , remainder='passthrough')
 x = np.array(ct.fit_transform(x))
-print("Features after encoding categorical data:")
-print(x)
+# print("Features after encoding categorical data:")
+# print(x)
+
+#encoding the Dependent Variable
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+# print("Target variable after encoding:")
+# print(y)
+
+
+#splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+print("Training set features (x_train):")
+print(x_train)
+print("Training set target variable (y_train):")
+print(y_train)
+print("Test set features (x_test):")
+print(x_test)
+print("Test set target variable (y_test):")
+print(y_test)
+
+#feature scaling with standardization
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])
+print("-" * 50)
+print ("Features after feature scaling:")
+print("Training set features (x_train) after scaling:")
+print(x_train)
+print("Test set features (x_test) after scaling:")
+print(x_test)
