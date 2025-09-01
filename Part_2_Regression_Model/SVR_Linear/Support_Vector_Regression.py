30d913439fb080f5981076358a22ee70b7f5027f
Salvatore Migliaccio <s.migliaccio@namirial.com>
2025-09-01 16:51:01 +0200

    Add Support Vector Regression implementation and visualization

--- Part_2_Regression_Model/SVR_Linear/Support_Vector_Regression.py
+++ Part_2_Regression_Model/SVR_Linear/Support_Vector_Regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
@@ -21,3 +22,30 @@ print(X)
 print("-" * 50)
 print(y)
 print("-" * 50)
+
+#training the SVR model on the whole dataset
+regressor = SVR(kernel='rbf')
+regressor.fit(X, y)
+
+#predict the new results
+results = sc_y.inverse_transform(regressor.predict(sc_x.transform([[6.5]])).reshape(-1, 1))
+print(results)
+
+
+#inverting the x and Y because we first transformed it
+plt.scatter(sc_x.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_x.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1, 1)), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#SVR results with more details and smoother curve
+X_grid = np.arange(min(sc_x.inverse_transform(X)), max(sc_x.inverse_transform(X)), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(sc_x.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
+plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_x.transform(X_grid)).reshape(-1,1)), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
